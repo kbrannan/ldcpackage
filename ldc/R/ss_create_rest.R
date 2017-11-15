0693a38bd10d1683aa6a228e650bdf5dcebcd231
kbrannan <brannan.kevin@deq.state.or.us>
2017-11-15 14:16:27 -0800

    function creates a rest quesry to send to stream stats

---
+++ ldc/R/ss_create_rest.R
@@ -0,0 +1,25 @@
+ss.create.rest <- function(x.loc = NULL,
+                           y.loc = NULL,
+                           ss.url ="https://streamstatsags.cr.usgs.gov/streamstatsservices",
+                           ss.prefix ="watershed",
+                           response.format = "geojson",
+                           rcode = "OR",
+                           crs=NULL,
+                           includeparameters = "false",
+                           includeflowtypes = "false",
+                           includefeatures = "true",
+                           simplify = "true") {
+  chr.REST <- paste0(ss.url, "/", ss.prefix, ".", response.format,
+                     "?rcode=", rcode,
+                     "&xlocation=", x.loc,
+                     "&ylocation=", y.loc,
+                     "&crs=", crs,
+                     "&includeparameters=", includeparameters,
+                     "&includeflowtypes=", includeflowtypes,
+                     "&includefeatures=", includefeatures,
+                     "&simplify=", simplify
+                     )
+
+  return(chr.REST)
+}
+
