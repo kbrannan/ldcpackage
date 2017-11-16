75d723400564732daa9f83484729a86e0cfa1cd5
kbrannan <brannan.kevin@deq.state.or.us>
2017-11-16 13:22:43 -0800

    added sommands to set the connection time out. this time out only covers the time to wait when there is no response from the server

--- ldc/R/ss_send_rest.R
+++ ldc/R/ss_send_rest.R
@@ -1,6 +1,7 @@
-ss.send.rest <- function(ss.REST.query=NULL) {
-  ss.responce <- try(RCurl::getURLContent(url = ss.REST.query),
-                     silent = TRUE)
+ss.send.rest <- function(ss.REST.query=NULL, connect.timeout = 180) {
+  ss.opts <- RCurl::curlOptions(connecttimeout = connect.timeout)
+  ss.responce <- try(RCurl::getURLContent(url = ss.REST.query,
+                     silent = TRUE, .opts = ss.opts))
   if(class(ss.responce) == "try-error") {
     return("Error in REST querry string")
   } else {
